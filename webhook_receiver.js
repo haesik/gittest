7dbb45268655d827e69f069242509cb8226df0fe
Ubuntu <ubuntu@ip-172-31-16-173.ap-northeast-2.compute.internal>
2018-06-05 06:38:58 +0000

    print commit contents

--- webhook_receiver.js
+++ webhook_receiver.js
@@ -12,10 +12,19 @@ var port = 8080, server;
 	} else if(request.method == 'POST') {
 		console.log('post request');	
 		request.on('data', function(data){
-			console.log('post data : ' + data);
-			obj = JSON.parse(data, function(key, value){
-				console.log(key + ":" + value);
-			});		
+			obj = JSON.parse(data);
+		//	, function(key, value){
+		//		console.log(key + ": " + value);
+		//	});		
+			console.log(' commits : ' + obj.commits);
+			console.log('post data : ' + JSON.stringify(obj, null, 2));
+			console.log(' commits.added :' + obj.commits.length);
+			obj.commits.forEach(function(commit){
+				console.log('commit.added.length : ' + commit.added.length);
+				commit.added.forEach(function(fname){
+					console.log('added filename : ' + fname);
+				});
+			});
 		});
 		status(response, "Hello,,, world... test... server post");
 	}
